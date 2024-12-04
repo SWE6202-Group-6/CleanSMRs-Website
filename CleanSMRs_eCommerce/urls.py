5f8b73606f84f48a1380651780e715cf0d1da918
Joel Heaton <joel.j.heaton@gmail.com>
2024-12-04 20:29:30 +0000

    Implement dynamic navbar with auth status

--- CleanSMRs_eCommerce/urls.py
+++ CleanSMRs_eCommerce/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("register/", views.register, name="register"),
     path("login/", views.log_in, name="login"),
+    path("logout/", views.log_out, name="logout"),
 ]
