f8dec361d470cebe59f6e8b1674bda8806641c2e
Joel Heaton <joel.j.heaton@gmail.com>
2024-12-04 20:51:48 +0000

    Implement custom user model and update registration form

--- CleanSMRs_eCommerce/forms.py
+++ CleanSMRs_eCommerce/forms.py
@@ -2,7 +2,8 @@
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+from .models import CustomUser
 
 
 class RegistrationForm(UserCreationForm):
@@ -13,12 +14,15 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         """Meta class for the RegistrationForm class."""
 
-        model = User
+        model = CustomUser
         fields = [
             "first_name",
             "last_name",
-            "username",
             "email",
+            "address",
+            "city",
+            "country",
+            "postal_code",
             "password1",
             "password2",
         ]
@@ -26,12 +30,16 @@ class RegistrationForm(UserCreationForm):
             "first_name": forms.TextInput(attrs={"required": True}),
             "last_name": forms.TextInput(attrs={"required": True}),
             "email": forms.EmailInput(attrs={"required": True}),
+            "address": forms.TextInput(attrs={"required": False}),
+            "city": forms.TextInput(attrs={"required": False}),
+            "country": forms.TextInput(attrs={"required": False}),
+            "postal_code": forms.TextInput(attrs={"required": False}),
         }
 
     def clean_email(self):
         """Validates that the email is unique."""
 
         email = self.cleaned_data.get("email")
-        if User.objects.filter(email=email).exists():
+        if CustomUser.objects.filter(email=email).exists():
             raise forms.ValidationError("Email is already in use")
         return email
--- CleanSMRs_eCommerce/models.py
+++ CleanSMRs_eCommerce/models.py
@@ -1,3 +1,33 @@
+"""Model definitions for the website."""
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .managers import CustomUserManager
+
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    """Custom user class that uses the email address as the username."""
+
+    username = None
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=255, blank=True, null=False)
+    city = models.CharField(max_length=255, blank=True, null=False)
+    country = models.CharField(max_length=255, blank=True, null=False)
+    postal_code = models.CharField(max_length=20, blank=True, null=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name",
+        "address",
+        "country",
+        "postal_code",
+    ]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
