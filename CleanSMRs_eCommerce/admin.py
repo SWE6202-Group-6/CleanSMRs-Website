ce95965d0130bb96592b09826dfeecd6ea0501c3
Joel Heaton <joel.j.heaton@gmail.com>
2024-12-05 10:19:50 +0000

    Add custom user model to the admin view

--- CleanSMRs_eCommerce/admin.py
+++ CleanSMRs_eCommerce/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import CustomUser
 
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    """Modifies the admin user display."""
+
+    ordering = ("first_name", "last_name")
+
+
+admin.site.register(CustomUser, UserAdmin)
