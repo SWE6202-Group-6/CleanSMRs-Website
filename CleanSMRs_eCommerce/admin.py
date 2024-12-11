1fa333ae11445bcba0f3fa7e759211db147c5cc7
Joel Heaton <joel.j.heaton@gmail.com>
2024-12-11 20:23:33 +0000

    Register models and custom admin views

--- CleanSMRs_eCommerce/admin.py
+++ CleanSMRs_eCommerce/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Order, Plan, Product, Subscription
 
 # Register your models here.
 
@@ -12,4 +12,48 @@ class UserAdmin(BaseUserAdmin):
     ordering = ("first_name", "last_name")
 
 
+class OrderAdmin(admin.ModelAdmin):
+    """Customises the admin display for the Order model."""
+
+    list_display = (
+        "id",
+        "order_number",
+        "date_placed",
+        "status",
+        "order_total",
+    )
+
+    # The order number shouldn't be editable.
+    readonly_fields = ("order_number",)
+
+
+class PlanAdmin(admin.ModelAdmin):
+    """Customises the admin display for the Plan model."""
+
+    list_display = ("id", "name", "duration_months")
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """Customises the admin display for the Product model."""
+
+    list_display = ("id", "name", "type", "price", "plan_id")
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    """Customises the admin display for the Subscription model."""
+
+    list_display = (
+        "id",
+        "plan_id",
+        "user_id",
+        "order_id",
+        "start_date",
+        "end_date",
+    )
+
+
 admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Plan, PlanAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
